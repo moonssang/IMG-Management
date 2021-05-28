8f463569c2d28636687c9270eabaae2574b021c2
H-H Moon <moonssang031@naver.com>
2021-05-28 22:54:18 +0900

    IMG pickme pickme up

--- app/src/main/java/com/smuexample/img_management/MainActivity.java
+++ app/src/main/java/com/smuexample/img_management/MainActivity.java
@@ -14,7 +14,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -42,77 +41,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         etName=findViewById(R.id.etName);
         etMsg=findViewById(R.id.etMsg);
-        iv=findViewById(R.id.iv);
+        iv=findViewById(R.id.iv1);
         select_btn = findViewById(R.id.select_btn);
         upload_btn = findViewById(R.id.upload_btn);
         load_btn = findViewById(R.id.load_btn);
 
-
+        //외부 저장소에 권한 필요, 동적 퍼미션
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
             int permissionResult= checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
             if(permissionResult== PackageManager.PERMISSION_DENIED){
-                String[] permissions= new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                String[] permissions= new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
                 requestPermissions(permissions,10);
             }
-        }else{
-            //cv.setVisibility(View.VISIBLE);
         }
 
-
-    }//onCreate() ..
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            case 10 :
-                if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
-                {
-                    Toast.makeText(this, "외부 메모리 읽기/쓰기 사용 가능", Toast.LENGTH_SHORT).show();
-
-                }else{
-                    Toast.makeText(this, "외부 메모리 읽기/쓰기 제한", Toast.LENGTH_SHORT).show();
-
-                }
-                break;
         }
-    }
+
 
 
     public void clickSelect(View view) {
 
         //갤러리 or 사진 앱 실행하여 사진을 선택하도록..
-        Intent intent= new Intent(Intent.ACTION_PICK);
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
         intent.setType("image/*");
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
-        //intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent,10);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         switch (requestCode){
             case 10:
                 if(resultCode==RESULT_OK){
+                    Toast.makeText(this, "RESULT_OK", Toast.LENGTH_SHORT).show();
                     Uri uri= data.getData();
                     if(uri!=null){
                         iv.setImageURI(uri);
+                        //갤러리앱에서 관리하는 DB정보가 있는데, 그것이 나온다 [실제 파일 경로가 아님!!]
+                        //얻어온 Uri는 Gallery앱의 DB번호임. (content://-----/2854)
+                        //업로드를 하려면 이미지의 절대경로(실제 경로: file:// -------/aaa.png 이런식)가 필요함
+                        //Uri -->절대경로(String)로 변환
+                        imgPath= getRealPathFromUri(uri);   //임의로 만든 메소드 (절대경로를 가져오는 메소드)
 
-                        imgPath= getRealPathFromUri(uri);
-
+                        //이미지 경로 uri 확인해보기
                         new AlertDialog.Builder(this).setMessage(uri.toString()+"\n"+imgPath).create().show();
                     }
-
-                }else
-                {
+                }else{
                     Toast.makeText(this, "이미지 선택을 하지 않았습니다.", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
-    }//onActivityResult() ..
+    }// onActivityResult()..
 
+    //Uri -- > 절대경로로 바꿔서 리턴시켜주는 메소드
     String getRealPathFromUri(Uri uri){
         String[] proj= {MediaStore.Images.Media.DATA};
         CursorLoader loader= new CursorLoader(this, uri, proj, null, null, null);
@@ -125,38 +108,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickUpload(View view) {
-
         String name= etName.getText().toString();
         String msg= etMsg.getText().toString();
 
-        String serverUrl="https://localhost/insertDB.php";
+        //안드로이드에서 보낼 데이터를 받을 php 서버 주소
+        String serverUrl="https://phpproject-cparr.run.goorm.io/workspace/PhpProject/insetDB.php";
 
 
+        //파일 전송 요청 객체 생성[결과를 String으로 받음]
         SimpleMultiPartRequest smpr= new SimpleMultiPartRequest(Request.Method.POST, serverUrl, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 new AlertDialog.Builder(MainActivity.this).setMessage("응답:"+response).create().show();
             }
-        }, new Response.ErrorListener() {
+        },new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(MainActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
             }
         });
 
+        //요청 객체에 보낼 데이터를 추가
         smpr.addStringParam("name", name);
         smpr.addStringParam("msg", msg);
-
+        //이미지 파일 추가
         smpr.addFile("img", imgPath);
 
+        //요청객체를 서버로 보낼 우체통 같은 객체 생성
         RequestQueue requestQueue= Volley.newRequestQueue(this);
         requestQueue.add(smpr);
 
     }
-
     public void clickLoad(View view) {
-
-        Intent intent= new Intent(this, TalkActivity.class);
-        startActivity(intent);
     }
 }
